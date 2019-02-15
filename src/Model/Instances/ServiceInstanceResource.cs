3d326fe6fc036aeb1582d54bd74641a246fc3e86
Ronald Wildenberg <rwwilden@gmail.com>
2019-02-15 22:24:53 +0100

    In ServiceInstanceProvisionRequest, both organization_guid and space_guid are now required Made plan_id an abstract property in ServiceInstanceBase so sub classes can specify for themselves whether plan_id is required ServiceInstanceUpdateRequest no longer extends from ServiceInstanceProvisionRequest but from ServiceInstanceBase to prevent plan_id from being required

--- src/Model/Instances/ServiceInstanceResource.cs
+++ src/Model/Instances/ServiceInstanceResource.cs
@@ -6,6 +6,12 @@ namespace OpenServiceBroker.Instances
     public class ServiceInstanceResource : ServiceInstanceBase, IEquatable<ServiceInstanceResource>
     {
         /// <summary>
+        /// The ID of the <see cref="Catalogs.Plan"/> from the catalog that is associated with the Service Instance.
+        /// </summary>
+        [JsonProperty("plan_id")]
+        public override string PlanId { get; set; }
+
+        /// <summary>
         /// The URL of a web-based management user interface for the Service Instance; we refer to this as a service dashboard. The URL MUST contain enough information for the dashboard to identify the resource being accessed.
         /// </summary>
         [JsonProperty("dashboard_url")]

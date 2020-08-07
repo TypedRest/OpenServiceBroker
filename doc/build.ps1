08f1d9c8e1d42dc4510a6f7ac9bdd92ef705afbb
Bastian Eicher <bastian@eicher.net>
2020-08-07 17:43:08 +0200

    Use 0install bootstrap scripts to run Doxygen

--- doc/build.ps1
+++ doc/build.ps1
@@ -2,20 +2,11 @@
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-# Ensure 0install is in PATH
-if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
-    echo "Downloading 0install"
-    mkdir -Force "$env:TEMP\zero-install" | Out-Null
-    Invoke-WebRequest "https://0install.de/files/0install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
-    $env:PATH = "$env:TEMP\zero-install;$env:PATH"
-}
-
 if (Test-Path ..\artifacts\Documentation) {rm -Recurse -Force ..\artifacts\Documentation}
 mkdir ..\artifacts\Documentation | Out-Null
 
-echo "Generating API documentation..."
 $env:VERSION = $Version
-0install run --batch https://apps.0install.net/devel/doxygen.xml
+.\_0install.ps1 run --batch https://apps.0install.net/devel/doxygen.xml
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 popd
--- doc/build.sh
+++ doc/build.sh
@@ -2,17 +2,8 @@
 set -e
 cd `dirname $0`
 
-# Ensure 0install is in PATH
-if ! command -v 0install > /dev/null 2> /dev/null; then
-    echo "Downloading 0install..."
-    download_dir=$(mktemp --directory)
-    curl -sSL https://get.0install.net/0install-$(uname | tr '[:upper:]' '[:lower:]')-$(uname -m)-latest.tar.bz2 | tar xj --strip-components=1 --directory=$download_dir
-    PATH=$PATH:$download_dir/files
-fi
-
 rm -rf ../artifacts/Documentation
 mkdir -p ../artifacts/Documentation
 
-echo "Generating API documentation..."
 VERSION=${1:-1.0-dev}
-0install run https://apps.0install.net/devel/doxygen.xml
+./_0install.sh run https://apps.0install.net/devel/doxygen.xml

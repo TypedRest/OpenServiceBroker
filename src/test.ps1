adb722cb54980f91cca916a2a3a1dc7da6faf3db
Bastian Eicher <bastian@eicher.net>
2020-08-16 13:52:14 +0200

    Report unit test results

--- src/test.ps1
+++ src/test.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-dotnet test --no-build --configuration Release UnitTests\UnitTests.csproj
+dotnet test --no-build --logger junit --configuration Release UnitTests\UnitTests.csproj
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 popd
--- src/test.sh
+++ src/test.sh
@@ -2,4 +2,4 @@
 set -e
 cd `dirname $0`
 
-dotnet test --no-build --configuration Release --framework netcoreapp3.1 UnitTests/UnitTests.csproj
+dotnet test --no-build --logger junit --configuration Release --framework netcoreapp3.1 UnitTests/UnitTests.csproj

6e7a52a274055187c5d839a05e47ac9b6e311cd2
Bastian Eicher <bastian@eicher.net>
2020-08-26 19:11:43 +0200

    Added InstanceUsable and UpdateRepeatable properties to BrokerException

--- src/Model/Errors/BrokerException.cs
+++ src/Model/Errors/BrokerException.cs
@@ -20,12 +20,24 @@ namespace OpenServiceBroker.Errors
         }
 
         /// <summary>
+        /// If an update or deprovisioning operation failed, this flag indicates whether or not the Service Instance is still usable. If true, the Service Instance can still be used, false otherwise. This field MUST NOT be present for errors of other operations. Defaults to true.
+        /// </summary>
+        public bool? InstanceUsable { get; set; }
+
+        /// <summary>
+        /// If an update operation failed, this flag indicates whether this update can be repeated or not. If true, the same update operation MAY be repeated and MAY succeed; if false, repeating the same update operation will fail again. This field MUST NOT be present for errors of other operations. Defaults to true.
+        /// </summary>
+        public bool? UpdateRepeatable { get; set; }
+
+        /// <summary>
         /// Serializes the exception to an error response object.
         /// </summary>
         public Error ToResponse() => new Error
         {
             ErrorCode = ErrorCode,
-            Description = Message
+            Description = Message,
+            InstanceUsable= InstanceUsable,
+            UpdateRepeatable = UpdateRepeatable
         };
 
         /// <summary>
@@ -33,43 +45,30 @@ namespace OpenServiceBroker.Errors
         /// </summary>
         public static BrokerException FromResponse(Error dto, HttpStatusCode statusCode)
         {
-            switch (dto.ErrorCode)
+            var exception = dto.ErrorCode switch
             {
-                case ApiVersionNotSupportedException.ErrorCode:
-                    return new ApiVersionNotSupportedException(dto.Description);
-                case AsyncRequiredException.ErrorCode:
-                    return new AsyncRequiredException(dto.Description);
-                case BadRequestException.ErrorCode:
-                    return new BadRequestException(dto.Description);
-                case ConcurrencyException.ErrorCode:
-                    return new ConcurrencyException(dto.Description);
-                case ConflictException.ErrorCode:
-                    return new ConflictException(dto.Description);
-                case GoneException.ErrorCode:
-                    return new GoneException(dto.Description);
-                case NotFoundException.ErrorCode:
-                    return new NotFoundException(dto.Description);
-                case RequiresAppException.ErrorCode:
-                    return new RequiresAppException(dto.Description);
-                case MaintenanceInfoConflictException.ErrorCode:
-                    return new MaintenanceInfoConflictException(dto.Description);
-            }
-
-            switch (statusCode)
-            {
-                case HttpStatusCode.PreconditionFailed:
-                    return new ApiVersionNotSupportedException(dto.Description);
-                case HttpStatusCode.BadRequest:
-                    return new BadRequestException(dto.Description);
-                case HttpStatusCode.Conflict:
-                    return new ConflictException(dto.Description);
-                case HttpStatusCode.Gone:
-                    return new GoneException(dto.Description);
-                case HttpStatusCode.NotFound:
-                    return new NotFoundException(dto.Description);
-            }
-
-            return new BrokerException(dto.ErrorCode, dto.Description);
+                ApiVersionNotSupportedException.ErrorCode => new ApiVersionNotSupportedException(dto.Description),
+                AsyncRequiredException.ErrorCode => new AsyncRequiredException(dto.Description),
+                BadRequestException.ErrorCode => new BadRequestException(dto.Description),
+                ConcurrencyException.ErrorCode => new ConcurrencyException(dto.Description),
+                ConflictException.ErrorCode => new ConflictException(dto.Description),
+                GoneException.ErrorCode => new GoneException(dto.Description),
+                NotFoundException.ErrorCode => new NotFoundException(dto.Description),
+                RequiresAppException.ErrorCode => new RequiresAppException(dto.Description),
+                MaintenanceInfoConflictException.ErrorCode => new MaintenanceInfoConflictException(dto.Description),
+                _ => statusCode switch
+                {
+                    HttpStatusCode.PreconditionFailed => new ApiVersionNotSupportedException(dto.Description),
+                    HttpStatusCode.BadRequest => new BadRequestException(dto.Description),
+                    HttpStatusCode.Conflict => new ConflictException(dto.Description),
+                    HttpStatusCode.Gone => new GoneException(dto.Description),
+                    HttpStatusCode.NotFound => new NotFoundException(dto.Description),
+                    _ => new BrokerException(dto.ErrorCode, dto.Description)
+                }
+            };
+            exception.InstanceUsable = dto.InstanceUsable;
+            exception.UpdateRepeatable = dto.UpdateRepeatable;
+            return exception;
         }
     }
 }

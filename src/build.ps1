e11eae29006a732ae1590f50d9327feed7d54293
Bastian Eicher <bastian@eicher.net>
2020-10-07 18:12:03 +0200

    Tweaked build scripts

--- src/build.ps1
+++ src/build.ps1
@@ -2,8 +2,16 @@
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-if ($env:CI) { $ci = "-p:ContinuousIntegrationBuild=True" }
-dotnet msbuild -v:Quiet -t:Restore -t:Build $ci -p:Configuration=Release -p:Version=$Version
-if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+function Run-DotNet {
+    if (Get-Command dotnet -ErrorAction SilentlyContinue) {
+        dotnet @args
+    } else {
+        ..\0install.ps1 run --batch --version 3.1..!3.2 https://apps.0install.net/dotnet/core-sdk.xml @args
+    }
+    if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+}
+
+if ($env:CI) { $ci = "/p:ContinuousIntegrationBuild=True" }
+Run-DotNet msbuild /v:Quiet /t:Restore /t:Build $ci /p:Configuration=Release /p:Version=$Version
 
 popd

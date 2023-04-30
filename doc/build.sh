042d0dab890d5765bef38e1ca8febd5a1ee41c86
Bastian Eicher <bastian@eicher.net>
2023-04-30 19:46:33 +0200

    Docs: Fixed DocFX command-line

--- doc/build.sh
+++ doc/build.sh
@@ -2,4 +2,4 @@
 set -e
 cd `dirname $0`
 
-../0install.sh run https://apps.0install.net/dotnet/docfx.xml --loglevel=warning --warningsAsErrors docfx.json
+../0install.sh run https://apps.0install.net/dotnet/docfx.xml --logLevel=warning --warningsAsErrors docfx.json

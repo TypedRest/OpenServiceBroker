45566d339f0c68f9bab8de684435cd924b2df367
Bastian Eicher <bastian@eicher.net>
2026-08-02 12:49:39 +0200

    Return Service Instance metadata for synchronous provisioning

--- src/Model/Instances/ServiceInstanceMetadata.cs
+++ src/Model/Instances/ServiceInstanceMetadata.cs
@@ -6,11 +6,19 @@ namespace OpenServiceBroker.Instances;
 /// <summary>
 /// Metadata for a Service Instance.
 /// </summary>
-public class ServiceInstanceMetadata
+public class ServiceInstanceMetadata : IEquatable<ServiceInstanceMetadata>
 {
     /// <summary>
     /// Broker specified key-value pairs specifying attributes of Service Instances that are meaningful and relevant to Platform users, but do not directly imply behaviour changes by the Platform.
     /// </summary>
     [JsonProperty("labels", NullValueHandling = NullValueHandling.Ignore)]
     public JObject Labels { get; set; }
+
+    public bool Equals(ServiceInstanceMetadata other)
+        => other != null
+        && JToken.DeepEquals(Labels, other.Labels);
+
+    public override bool Equals(object obj) => obj is ServiceInstanceMetadata other && Equals(other);
+
+    public override int GetHashCode() => Labels?.GetHashCode() ?? 0;
 }
--- src/Model/Instances/ServiceInstanceProvision.cs
+++ src/Model/Instances/ServiceInstanceProvision.cs
@@ -10,12 +10,19 @@ public class ServiceInstanceProvision : IUnchangedFlag, IEquatable<ServiceInstan
     [JsonProperty("dashboard_url")]
     public Uri DashboardUrl { get; set; }
 
+    /// <summary>
+    /// An optional object containing metadata for the Service Instance.
+    /// </summary>
+    [JsonProperty("metadata", NullValueHandling = NullValueHandling.Ignore)]
+    public ServiceInstanceMetadata Metadata { get; set; }
+
     [JsonIgnore]
     public bool Unchanged { get; set; }
 
     public bool Equals(ServiceInstanceProvision other)
         => other != null
         && DashboardUrl == other.DashboardUrl
+        && Equals(Metadata, other.Metadata)
         && Unchanged == other.Unchanged;
 
     public override bool Equals(object obj) => obj is ServiceInstanceProvision other && Equals(other);
@@ -24,7 +31,10 @@ public class ServiceInstanceProvision : IUnchangedFlag, IEquatable<ServiceInstan
     {
         unchecked
         {
-            return ((DashboardUrl != null ? DashboardUrl.GetHashCode() : 0) * 397) ^ Unchanged.GetHashCode();
+            int hashCode = (DashboardUrl != null ? DashboardUrl.GetHashCode() : 0) * 397;
+            hashCode = (hashCode * 397) ^ (Metadata?.GetHashCode() ?? 0);
+            hashCode = (hashCode * 397) ^ Unchanged.GetHashCode();
+            return hashCode;
         }
     }
 }
